6e1d287f903b00d6c3e24b35f9be408eb4d39ec4
ankurksoni <ankursoni.leonardo@gmail.com>
2023-06-14 10:12:21 +0530

    chores: basic setup

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true,
+      typePaths: ["./**/*.graphql"]
+    }),
+    BookModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
